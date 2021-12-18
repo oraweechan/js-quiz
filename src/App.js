6e5852700a58a1f39921166a5c0afb7b377545a2
“Orawee <oraweechan@gmail.com>
2021-12-18 09:49:40 -0800

    added bootstrap, and created sign in and signup screens

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Intro from "./pages/Intro";
-import Question from "./pages/Question";
-import Result from "./pages/Result";
+
+import Intro from "./screens/Start";
+import Question from "./screens/Question";
+import Result from "./screens/Result";
+import { Routes, Route, Link } from "react-router-dom";
+import UserContext from "./screens/auth/userContext";
+import React, { useState } from "react";
+
+
 
 import { Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-
+import SignIn from "./screens/auth/SignIn";
+import SignUp from "./screens/auth/SignUp";
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+
+
+
   return (
-    <Router>
-    <Container maxWidth="sm">
-      <Box textAlign="center" mt={5}>
-        <Typography variant="h2" fontWeight="bold">
-                  Quiz App
-        </Typography>
-        
-        <Intro/>
-        <Question/>
-        <Result/>
-  
-      </Box>
-    </Container>
-  </Router>
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Typography variant="h2" fontWeight="bold">
+                    Quiz App
+          </Typography>
+
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="start" element={<Intro />} />
+            <Route path="play" element={<Question />} />
+            <Route path="results" element={<Result />} />
+          </Routes>
+
+        </Box>
+      </Container>
+    </UserContext.Provider>
+ 
   );
 }
 
---
+++ src/screens/Start.js
@@ -0,0 +1,11 @@
+function Start() {
+    return (
+      <div className="Start">
+          <h1>Start</h1>
+  
+      </div>
+    );
+  }
+  
+  export default Start;
+  
